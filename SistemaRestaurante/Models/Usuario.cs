5ad504d5f53b6c1b34e234dab52d6f535a84204a
Gabriel Viana <gaa.vianaa@gmail.com>
2026-07-28 19:07:58 -0300

    model usuario

---
+++ SistemaRestaurante/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using SistemaRestaurante.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaRestaurante.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public PerfilUsuario Perfil { get; set; }
+    }
+}
